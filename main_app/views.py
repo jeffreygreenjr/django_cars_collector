8d3109fa50b64c17813304dd6f128ac2c00ace92
jeffreygreen <mrjeffreygreen@gmail.com>
2022-05-27 13:30:57 -0500

    Implemented Template Inheritance

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse # <- a class to handle sending a type of response
+#...
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+# Here we will be creating a class called Home and extending it from the Template View class
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
