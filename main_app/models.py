fcbb636003aab5d96a64fc82a1fd2cf446998efb
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-01 13:43:12 -0500

    Added styling throughout the app

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,7 @@ class Car(models.Model):
 
     name = models.CharField(max_length=100)
     year = models.TextField(max_length=4)
+    bio = models.TextField(max_length=500, default='Car Bio')
     img = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     
