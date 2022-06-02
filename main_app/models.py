bd70080043fdac40f825d27cba2125116c6840f6
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-02 15:15:51 -0500

    Added login and logout functionality for authentication

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=150)
     location = models.CharField(max_length=150)
+      # Add a relationship for the user
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     def __str__(self):
         return self.name
+    class Meta:
+        ordering = ['name']
+
 
 class VehicleType(models.Model):
     name = models.CharField(max_length=150)
+
     def __str__(self):
         return self.name
+        
 
 class Car(models.Model):
     name = models.CharField(max_length=100)
@@ -21,6 +29,7 @@ class Car(models.Model):
     bio = models.TextField(max_length=500)
     img = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
 
