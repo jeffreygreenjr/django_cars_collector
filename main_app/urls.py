ab543265ebd273984b050b89917c49704d6d6d23
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-03 09:47:30 -0500

    Added a signup feature for new users

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('cars/<int:pk>/', views.CarDetail.as_view(), name="car_detail"),
     path('cars/<int:pk>/update',views.CarUpdate.as_view(), name="car_update"),
     path('cars/<int:pk>/delete',views.CarDelete.as_view(), name="car_delete"),
+    path('accounts/signup/', views.Signup.as_view(), name="signup")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.urls import reverse
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse # <- a class to handle sending a type of response
 
 from django.views import View # <- View class to handle requests
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import DetailView
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Car
 
@@ -19,33 +22,6 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-
- #adds artist class for mock database data
-# class Car:
-#     def __init__(self, name, year, image):
-#         self.name = name
-#         self.year = year
-#         self.image = image
-
-# cars = [
-#     Car("Gorillaz", 
-#         "2022",
-#         "https://i.scdn.co/image/ab67616d00001e0295cf976d9ab7320469a00a29"),
-#     Car("Panic! At The Disco",
-#         "2003",
-#         "https://i.scdn.co/image/58518a04cdd1f20a24cf0545838f3a7b775f8080"),
-#     Car("Joji", 
-#         "2020", 
-#         "https://i.scdn.co/image/7bc3bb57c6977b18d8afe7d02adaeed4c31810df"),
-# ]
-
-# class CarList(TemplateView):
-#     template_name = "car_list.html"
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context["cars"] = Car.objects.all() # this is where we add the key into our context object for the view to use
-#         return context
 
 class CarList(TemplateView):
     template_name = "car_list.html"
@@ -66,6 +42,7 @@ class CarList(TemplateView):
             context["header"] = "Trending Cars"
         return context
 
+
 class MyCarList(TemplateView):
     template_name = "my_car_list.html"
 
@@ -85,6 +62,7 @@ class MyCarList(TemplateView):
             # default header for not searching 
             context["header"] = "Trending Cars"
         return context
+
 
 class CarCreate(CreateView):
     model = Car
@@ -117,3 +95,21 @@ class CarDelete(DeleteView):
     model = Car
     template_name = "car_delete_confirmation.html"
     success_url = "/cars/"
+
+
+class Signup(View):
+    # show a form to fill out
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+    # on form ssubmit validate the form and login the user.
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("car_list")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
